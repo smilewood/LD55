aa5346249c6fc28174f4a006063f8b46471937cb
RileyWood938 <71392554+RileyWood938@users.noreply.github.com>
2024-04-13 22:31:39 -0600

    Pixelization Shader
    Added a cool shader to get pixel appearing

--- Assets/Assets/rendererFeature.cs
+++ Assets/Assets/rendererFeature.cs
@@ -13,27 +13,21 @@ namespace LD55
         public class CustomPassSettings
         {
             public RenderPassEvent passEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+            public int screenHeight = 144;
+        }
+        [SerializeField] private CustomPassSettings customPassSettings;
+        private PixelizePass customPass;
+        public override void Create()
+        {
+            customPass = new PixelizePass(customPassSettings);
         }
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
         {
-            throw new System.NotImplementedException();
+#if UNITY_EDITOR
+            if (renderingData.cameraData.isSceneViewCamera) return;
+#endif
+            renderer.EnqueuePass(customPass);
         }
 
-        public override void Create()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
