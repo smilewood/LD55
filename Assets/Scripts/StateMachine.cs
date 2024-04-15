fe51e4707cbbb342f11c0f562b8b5ea2f3476778
Ryan <rkilbride13@gmail.com>
2024-04-14 18:10:30 -0600

    Normalized player movement so diag isn't faster

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LD55
+{
+    public interface IState
+    {
+        public bool CheckStateTransition(IState nextState);
+
+        public void Start();
+        public void Update();
+        public void OnStateExit();
+
+    }
+}
